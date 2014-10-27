e05e279a992601ef2f7185b547639554a0876972
Arjan Frans <arjan.frans1993@gmail.com>
2014-10-27 20:14:40 +0100

    updated libgdx and gradle

--- android/src/nl/arjanfrans/maze/android/AndroidLauncher.java
+++ android/src/nl/arjanfrans/maze/android/AndroidLauncher.java
@@ -1,6 +1,7 @@
 package nl.arjanfrans.maze.android;
 
 import android.os.Bundle;
+
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import nl.arjanfrans.maze.MasterMaze;
@@ -10,7 +11,6 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-
 		initialize(new MasterMaze(), config);
 	}
 }
--- ios/src/nl/arjanfrans/maze/IOSLauncher.java
+++ ios/src/nl/arjanfrans/maze/IOSLauncher.java
@@ -1,9 +1,11 @@
 package nl.arjanfrans.maze;
 
-import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
-import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
 import org.robovm.apple.foundation.NSAutoreleasePool;
 import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import nl.arjanfrans.maze.MasterMaze;
 
 public class IOSLauncher extends IOSApplication.Delegate {
     @Override
